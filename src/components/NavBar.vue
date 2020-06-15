aa867b3dcd44507fac6ede35a97a754214a1075f
adrianFEdev <62696850+adrianFEdev@users.noreply.github.com>
2020-06-15 10:14:59 +0200

    Add files via upload

---
+++ src/components/NavBar.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav class="main-wrapper">
+    <div class="nav-wrapper">
+      <div class="logo-wrapper">
+        <a @click="enterHome" class="brand-logo">Decathlon</a>
+      </div>
+      <ul class="right">
+        <li>
+          <a>
+            <i class="material-icons md-dark">search</i>
+          </a>
+        </li>
+        <li>
+          <a>
+            <i class="material-icons md-dark">help_outline</i>
+          </a>
+        </li>
+        <li>
+          <a @click="log">
+            <i class="material-icons md-dark">store</i>
+          </a>
+        </li>
+        <li>
+          <a>
+            <i class="material-icons md-dark">perm_identity</i>
+          </a>
+        </li>
+        <li>
+          <a @click="enterFavorite">
+            <i class="material-icons md-dark">favorite</i>
+          </a>
+        </li>
+        <li>
+          <a>
+            <i class="material-icons md-dark">shopping_cart</i>
+          </a>
+        </li>
+      </ul>
+      <ul class="left">
+        <li>
+          <a href="#">
+            <i class="material-icons md-dark">menu</i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      localFavorite: null
+    };
+  },
+  methods: {
+    enterFavorite() {
+      this.$router.push({ name: "Favorite", params: { favoriteList: [] } });
+    },
+    enterHome() {
+      this.$router.push({ name: "Welcome", params: {} });
+    },
+    log () {
+        console.log(this.localFavorite)
+        location.reload()
+    }
+  },
+  created: function() {
+
+  }
+};
+</script>
+
+<style scoped>
+.main-wrapper {
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+}
+.brand-logo {
+  position: absolute;
+  width: 20%;
+  height: auto;
+  background-color: rgb(0, 225, 255);
+}
+.material-icons.md-dark {
+  color: rgb(0, 0, 0);
+}
+.material-icons.md-dark.md-inactive {
+  color: rgba(0, 0, 0, 0.26);
+}
+
+.material-icons.md-light {
+  color: rgba(255, 255, 255, 1);
+}
+.material-icons.md-light.md-inactive {
+  color: rgba(255, 255, 255, 0.3);
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div class="content">
+      <Navbar/>
+      <CategoryMen/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/NavBar'
+import CategoryMen from '@/components/Category-Men'
+
+export default {
+  name: 'Welcome',
+  components: {
+        Navbar,
+        CategoryMen,
+      },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
