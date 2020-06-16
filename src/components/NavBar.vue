0bd94a9f977405d83914d1433cef606f89646a11
adrianFEdev <62696850+adrianFEdev@users.noreply.github.com>
2020-06-16 15:30:20 +0200

    Add files via upload

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,44 +4,19 @@
       <div class="logo-wrapper">
         <a @click="enterHome" class="brand-logo">Decathlon</a>
       </div>
-      <ul class="right">
-        <li>
-          <a>
-            <i class="material-icons md-dark">search</i>
-          </a>
-        </li>
-        <li>
-          <a>
-            <i class="material-icons md-dark">help_outline</i>
-          </a>
-        </li>
-        <li>
-          <a @click="clearCurrentSession">
-            <i class="material-icons md-dark">store</i>
-          </a>
-        </li>
-        <li>
-          <a>
-            <i class="material-icons md-dark">perm_identity</i>
-          </a>
-        </li>
-        <li>
-          <a @click="enterFavorite">
-            <i class="material-icons md-dark">favorite</i>
-          </a>
-        </li>
-        <li>
-          <a>
-            <i class="material-icons md-dark">shopping_cart</i>
-          </a>
-        </li>
+      <ul class="right icon-wrapper">
+        <li><a><i class="material-icons md-dark suchen">search</i></a></li>
+        <li><a><i class="material-icons md-dark hilfe">help_outline</i></a>
+        <li><a @click="clearCurrentSession"><i class="material-icons md-dark laden">store</i></a></li>
+        <li><a><i class="material-icons md-dark kunde">perm_identity</i></a></li>
+        <li><a @click="enterFavorite"><i class="material-icons md-dark favoriten">favorite</i></a></li>
+        <li><a><i class="material-icons md-dark einkauf">shopping_cart</i></a></li>
       </ul>
+
+     
+
       <ul class="left">
-        <li>
-          <a href="#">
-            <i class="material-icons md-dark">menu</i>
-          </a>
-        </li>
+        <li><a href="#"><i class="material-icons md-dark">menu</i></a></li>
       </ul>
     </div>
   </nav>
@@ -73,27 +48,48 @@ export default {
 </script>
 
 <style scoped>
+
 .main-wrapper {
   background-color: rgb(255, 255, 255);
   width: 100%;
 }
+
+.logo-wrapper {
+  margin-right: 90%;
+}
+
 .brand-logo {
   position: absolute;
   width: 20%;
   height: auto;
-  background-color: rgb(68, 68, 68);
+  background-color: rgb(3, 159, 221);
 }
+
 .material-icons.md-dark {
   color: rgb(0, 0, 0);
 }
-.material-icons.md-dark.md-inactive {
-  color: rgba(0, 0, 0, 0.26);
+
+@media screen and (max-width: 1120px) {
+nav .brand-logo {
+    left: 18%;
 }
 
-.material-icons.md-light {
-  color: rgba(255, 255, 255, 1);
+.hilfe, .laden {
+display: none;  
+  }
 }
-.material-icons.md-light.md-inactive {
-  color: rgba(255, 255, 255, 0.3);
+
+@media screen and (max-width: 750px) {
+
+.hilfe, .laden {
+display: none;  
+}
+  }
+
+@media screen and (max-width: 425px) {
+
+ .hilfe, .laden {
+display: none; 
+  }
 }
 </style>
